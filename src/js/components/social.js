cf42145cda566939d49cc01993add21a7c7bcc7c
anrepo <andrey.polenok@gmail.com>
2022-09-22 10:27:46 +0300

    Start Dynamic Share

--- src/js/components/social.js
+++ src/js/components/social.js
@@ -1,4 +1,25 @@
 import Share from 'ninelines-sharing';
+// import html2canvas from 'html2canvas';
+// import { saveAs } from 'file-saver';
+
+function changeShareData() {
+	$('meta[property="og:title"]').attr('content', $('.share span.name').text());
+	$('meta[property="og:description"]').attr('content', $('.share span.position').text());
+
+	$('meta[property="twitter:title"]').attr('content', $('.share span.name').text());
+	$('meta[property="twitter:description"]').attr('content', $('.share span.position').text());
+}
+
+// function createCanvas() {
+// 	console.log('Canvas');
+// 	html2canvas(document.querySelector('.share-box')).then(function(canvas) {
+// 		document.body.appendChild(canvas);
+
+// 		canvas.toBlob(function(blob) {
+// 			saveAs(blob, "/images/share.png");
+// 		});
+// 	});
+// }
 
 if (document.querySelector('[data-social]')) {
 	const list = document.querySelectorAll('[data-social]');
@@ -7,6 +28,9 @@ if (document.querySelector('[data-social]')) {
 		item.addEventListener('click', (e) => {
 			const social = e.currentTarget.dataset.social;
 			const url = location.origin + location.pathname;
+
+			// createCanvas();
+			changeShareData();
 
 			Share[social](url);
 		});
